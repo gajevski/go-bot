6d1b4be6fa57145819a8abe10db5c0acec1e74be
gajevski <mikolajxgajewski@gmail.com>
2024-06-12 22:44:22 +0200

    modules communication

---
+++ bot/bot.go
@@ -0,0 +1,9 @@
+package bot
+
+import (
+  "fmt"
+)
+
+func CallBot()  {
+ fmt.Println("hello from bot package") 
+}
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
   "fmt"
+  "go-bot/bot"
 )
 
 func main() {
   fmt.Println("Hello world")
+  bot.CallBot()
 }
