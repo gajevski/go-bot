0e96cca631571fd5e2ef11e109d8375ea47c1a56
gajevski <mikolajxgajewski@gmail.com>
2024-06-13 14:04:59 +0200

    test bot message

--- bot/bot.go
+++ bot/bot.go
@@ -2,10 +2,37 @@ package bot
 
 import (
   "fmt"
+  "log"
+  "os"
+  "os/signal"
+  "strings"
+  "github.com/bwmarrin/discordgo"
 )
 
 var BotToken string
 
 func Start()  {
  fmt.Println(BotToken)
+ discord, err := discordgo.New("Bot " + BotToken)
+ if err != nil {
+   log.Fatal("Error")
+ }
+ discord.AddHandler(newMessage)
+ discord.Open()
+ defer discord.Close()
+ fmt.Println("Bot running....")
+ c := make(chan os.Signal, 1)
+ signal.Notify(c, os.Interrupt)
+ <-c
+}
+
+func newMessage(discord *discordgo.Session, message *discordgo.MessageCreate)  {
+  /* Prevent bot from respoding to its own messages */
+  if message.Author.ID == discord.State.User.ID {
+    return
+  }
+  switch {
+  case strings.Contains(message.Content, "!help"):
+    discord.ChannelMessageSend(message.ChannelID, "I'm bot, I'm helping!")
+  }
 }
