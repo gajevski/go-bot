154e7a2d9158b2cf18c8fa699e15984b8ab12be4
gajevski <mikolajxgajewski@gmail.com>
2024-06-13 13:36:49 +0200

    start method to init bot

--- bot/bot.go
+++ bot/bot.go
@@ -4,6 +4,6 @@ import (
   "fmt"
 )
 
-func CallBot()  {
+func Start()  {
  fmt.Println("hello from bot package") 
 }
--- main.go
+++ main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-  "fmt"
   "go-bot/bot"
 )
 
 func main() {
-  fmt.Println("Hello world")
-  bot.CallBot()
+  bot.Start()
 }
